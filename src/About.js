555c5c3fe774bce6c76c353c4d789c01e0df64d5
Savan <githost.2007@gmail.com>
2024-07-07 15:15:17 +0530

    Update About.js

--- src/About.js
+++ src/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import aimg1 from './Images/IMG_20240629_184836.jpg';
+import aimg1 from './Images/IMG_20240706_143509.jpg';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 const About =()=>{
@@ -15,11 +15,7 @@ const About =()=>{
     </div>
     <div className="col-md-8 d-flex justify-content-center align-items-center">
       <div className="card-body">
-        <p className="card-text lh-base">•Passionate Storytellers: We specialize in capturing unique stories and emotions through photography.
-•Creative Excellence: Combining artistic vision with technical expertise for exceptional results.
-•Tailored Approach: Customized sessions to reflect your individual style and preferences.
-•Memorable Moments: Creating timeless images that evoke emotions and preserve memories.
-•Client Satisfaction: Our priority is ensuring your photography experience is seamless and satisfying.</p>
+        <p className="card-text fs-3 text-center lh-lg">we specialize in capturing the unforgettable moments of your most cherished events. With years of experience and a passion for storytelling through images, we aim to provide you with beautiful, timeless photographs that you will treasure forever.</p>
 
       </div>
     </div>
